dac7ab715dda634251031a0603c428e58a1d16e9
Harris Schneiderman <harris.schneiderman@deque.com>
2017-11-07 10:59:34 -0500

    chore: adds some eslint rules

--- react.js
+++ react.js
@@ -31,6 +31,7 @@ module.exports = _.mergeWith(parent, {
     'react/jsx-tag-spacing': 2,
     'react/jsx-first-prop-new-line': [2, 'multiline'],
     'react/jsx-equals-spacing': [2, 'never'],
-    'react/jsx-no-literals': 2
+    'react/jsx-no-literals': 2,
+    'react/jsx-curly-spacing': { 'when': 'never', 'children': true }
   }
 }, merger);
